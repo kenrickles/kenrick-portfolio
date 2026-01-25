3e41fe5e5fa7e6e482c91f49eb4d9adac368d1c6
Kenbot <kenbot@Kenbots-Virtual-Machine.local>
2026-01-25 12:17:09 -0800

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const repo = 'kenrick-portfolio';
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
+};
 
 export default nextConfig;
